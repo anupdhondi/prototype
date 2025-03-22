1b5d5b52af2fbbfc18a4a0edf642a4db6effaa8e
Anup Dhondi <anupdhondi5@gmail.com>
2025-03-23 00:12:52 +0530

    Native prototype

--- 3.NativePrototypes/nativePrototype.js
+++ 3.NativePrototypes/nativePrototype.js
@@ -25,3 +25,29 @@ console.log(Boolean.prototype, String.prototype, Number.prototype);
 // Special values null and undefined stand apart.
 // They have no object wrappers, so methods and properties are not available for them.
 // And there are no corresponding prototypes either.
+
+function f() {
+  console.log("hello");
+}
+Function.prototype.defer = function (time) {
+  setTimeout(this, time);
+};
+f.defer(1000);
+
+function b() {
+  console.log("Bhow");
+}
+b.defer(1000);
+
+function fc(a, b) {
+  alert(a + b);
+}
+
+Function.prototype.defer2 = function (time) {
+  let f = this;
+  return function (...args) {
+    return setTimeout(() => f.apply(this, args), time);
+  };
+};
+
+console.log(fc.defer2(1000)(1, 2));
