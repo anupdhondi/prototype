23bfb16e7bc0703d6dd543897ec57e7006428ccd
Anup Dhondi <anupdhondi5@gmail.com>
2025-03-22 21:35:03 +0530

    Limitations of prototypical inheritence

--- prototypicalInheritence.js
+++ prototypicalInheritence.js
@@ -10,3 +10,30 @@ function prototypicalInheritence() {
   console.log(rabbit.eats);
 }
 prototypicalInheritence();
+
+//The prototype chain can be longer:
+let animal = {
+  eats: true,
+  walk() {
+    console.log("Animal walk");
+  },
+};
+
+let rabbit = {
+  jumps: true,
+  __proto__: animal,
+};
+
+let longEar = {
+  earLength: 10,
+  __proto__: rabbit,
+};
+
+// walk is taken from the prototype chain
+longEar.walk(); // Animal walk
+console.log(longEar.jumps); // true (from rabbit)
+
+// There are only two limitations:
+
+// The references can’t go in circles. JavaScript will throw an error if we try to assign __proto__ in a circle.
+// The value of __proto__ can be either an object or null. Other types are ignored.
